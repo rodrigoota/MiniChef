64fe417153a471e14c0dd158278d4a5eae02f974
rodrigoota <rodrigotety@hotmail.com>
2015-04-10 19:55:17 -0300

    Inicio

---
+++ MiniChef/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MiniChef.Resources;
+
+namespace MiniChef
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ MiniChef/Model/ReceitaModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChef.Model
+{
+    public class ReceitaModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public DateTime data { get; set; }
+        //private List<IngredienteVO> ingredientes;
+        public int tempo { get; set; }
+        public int nota { get; set; }
+        public string categoria { get; set; }
+        //private List<CategoriaVO> categorias;
+        public string foto { get; set; }
+    }
+}
