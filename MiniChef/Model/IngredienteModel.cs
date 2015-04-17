5ab1aa9728c4b18d99549e86de71459eddd53834
rodrigoota <rodrigotety@hotmail.com>
2015-04-17 10:38:47 -0300

    Models que faltaram

---
+++ MiniChef/Model/IngredienteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniChef.Model
+{
+    public class IngredienteModel
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+        public double quantidade { get; set; }
+        public string unidadeMedida { get; set; }
+    }
+}
